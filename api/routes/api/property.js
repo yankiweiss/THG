e5db9809cac384ff8c7e3690b52ea5c6d2271c27
yankiweiss <yakovw2706@gmail.com>
2026-01-19 18:00:55 -0500

    updating the addDeal route

--- api/routes/api/property.js
+++ api/routes/api/property.js
@@ -3,7 +3,7 @@ import { postAProperty } from '../../controllers/propertyController';
 const router = express.Router();
 
 
-router.route('/').post('/addDeal' , postAProperty)
+router.route('/addDeal').post(postAProperty)
 
 
 export default router;
