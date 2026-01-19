4c06c3cae5df50d2fd44809a99d7afddf5fd22d4
yankiweiss <yakovw2706@gmail.com>
2026-01-19 18:13:13 -0500

    update

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 import cors from 'cors'
 import router from './routes/api/property.js';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const PORT = process.env.PORT || 3000;
 
