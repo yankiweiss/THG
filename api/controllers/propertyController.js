c1c4a8888b648d69ba12c2adb7beb44df179cdbf
yankiweiss <yakovw2706@gmail.com>
2026-01-20 13:44:37 -0500

    update

--- api/controllers/propertyController.js
+++ api/controllers/propertyController.js
@@ -1,64 +1,66 @@
 import dataBasePool from "../model/db.js";
 
 const postAProperty = async (req, res) => {
-    const { property_name, purchase_price, syndicator } = req.body;
+  const { property_name, purchase_price, syndicator } = req.body;
 
-    const client = await dataBasePool.connect();
+  const client = await dataBasePool.connect();
 
-    try {
-        await client.query("BEGIN");
+  try {
+    await client.query("BEGIN");
 
-        const putInPropertyTable = `
+    const putInPropertyTable = `
   INSERT INTO properties(property_name, purchase_price )
   VALUES($1, $2 ) RETURNING id`;
 
-        const propertyResult = await client.query(putInPropertyTable, [
-            property_name,
-            purchase_price,
-        ]);
+    const propertyResult = await client.query(putInPropertyTable, [
+      property_name,
+      purchase_price,
+    ]);
 
-        const propertyID = propertyResult.rows[0].id;
+    const propertyID = propertyResult.rows[0].id;
 
-        const putInInvestorTable = `
+    const putInInvestorTable = `
  INSERT INTO investor (investor_name, property_id)
  VALUES($1, $2)`;
 
-        for (let investor of syndicator) {
-            await client.query(putInInvestorTable, [
-                investor.investor_name,
-                propertyID,
-            ]);
-        }
-
-        await client.query("COMMIT");
-    } catch (error) {
-        console.error(error);
-    } finally {
-        client.release();
+    for (let investor of syndicator) {
+      await client.query(putInInvestorTable, [
+        investor.investor_name,
+        propertyID,
+      ]);
     }
+
+    await client.query("COMMIT");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.release();
+  }
 };
 
-const getAllProperties = async (req, res) =>  {
-
-    
-    const getAllPropertiesDB = `
+const getAllProperties = async (req, res) => {
+  const getAllPropertiesDB = `
     SELECT * FROM properties`;
 
-    const result = await dataBasePool.query(getAllPropertiesDB)
+  const result = await dataBasePool.query(getAllPropertiesDB);
 
-    res.json(result.rows
-)
-
-}
+  res.json(result.rows);
+};
 
 const getPropertyById = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    const query = ` SELECT * FROM properties WHERE id = $1`;
+  const propertyResult = await dataBasePool.query(
+    `SELECT * FROM properties WHERE id = $1`,
+    [id],
+  );
 
-    const result = await dataBasePool.query(query, [id])
+  const investorResult = await dataBasePool.query(
+    "SELECT FROM investors WHERE property_id = $1",
+    [id],
+  );
 
-    res.json(result.rows[0])
-}
+  res.json({ ...propertyResult.rows[0], investors: investorResult.rows });
+};
 
-export { postAProperty, getAllProperties , getPropertyById};
+export { postAProperty, getAllProperties, getPropertyById };
--- client/src/PropertyDetail.jsx
+++ client/src/PropertyDetail.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function PropertyDetail() {
-  const [property, setProperty] = useState(null);
+  const [property, setProperty] = useState([]);
 
   const { id } = useParams();
 
@@ -14,23 +14,59 @@ function PropertyDetail() {
       .catch((err) => console.error(err));
   }, [id]); // ✅ dependency array
 
-  console.log(property)
-
   return (
     <>
       <div className="container my-5">
         {/* ================= Patient Info ================= */}
+
+        <h4 className="m-5">{property.property_name}</h4>
         <div className="card shadow-sm mb-4">
-          <div className="card-header bg-light fw-bold">Properties</div>
+          <div className="card-header bg-light fw-bold">Property Details</div>
 
           <div className="card-body">
+            <div className="row">
+              <div className="col-md-2">
+                <label className="form-label">Property Name</label>
+                <input
+                  className="form-control"
+                  value={property.property_name}
+                ></input>
+              </div>
 
-        
-          
-         
-    
-      </div>
-      </div>
+              <div className="col-md-2">
+                <label className="form-label">Purchase Price</label>
+                <input
+                  className="form-control"
+                  value={property.purchase_price}
+                ></input>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card shadow-sm mb-4">
+          <div className="card-header bg-light fw-bold">Investor Details</div>
+
+          <div className="card-body">
+            <div className="row">
+              <div className="col-md-2">
+                <label className="form-label">Investor Name</label>
+                <input
+                  className="form-control"
+                 
+                ></input>
+              </div>
+
+              <div className="col-md-2">
+                <label className="form-label">Amount Invested</label>
+                <input
+                  className="form-control"
+                
+                ></input>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
