847d63c70e6dfd5a88fddd55481801ea062a412f
yankiweiss <yakovw2706@gmail.com>
2026-01-20 16:11:11 -0500

    update

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -4,7 +4,7 @@ const postAEvent = async (req, res) => {
   const { event_date, event_type, event_amount, investor_id } = req.body;
 
   const postEvent = `
-  INSERT INTO events (event_date,event_type, event_type, investor_id) VALUES($1, $2, $3, $4)`
+  INSERT INTO events (event_date,event_type, event_amount, investor_id) VALUES($1, $2, $3, $4)`
 
   await dataBasePool.query(postEvent, [event_date, event_type, event_amount, investor_id])
 
